2df6a8e08c89c8236452acd77be96773cbbfa316
derekhuang <derekhuanggames@gmail.com>
2015-06-24 10:54:08 -0700

    * initialcommitment

---
+++ src/com/derekhuang/Game/Game.java
@@ -0,0 +1,32 @@
+package com.derekhuang.Game;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Game extends BasicGame{
+
+	public Game(String title) {
+		super(title);
+
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		
+	}
+
+	@Override
+	public void init(GameContainer container) throws SlickException {
+		
+	}
+
+	@Override
+	public void update(GameContainer container, int delta)
+			throws SlickException {
+		
+	}
+
+}
---
+++ src/com/derekhuang/Game/Main.java
@@ -0,0 +1,7 @@
+package com.derekhuang.Game;
+
+public class Main {
+	public static void main(String args[]) {
+		
+	}
+}
