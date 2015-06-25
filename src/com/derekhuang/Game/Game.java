467b001c8a67ee63db6142db493851fb4c3c34e2
csmlab <csmlab@SII321-31.ad.csufresno.edu>
2015-06-25 09:35:49 -0700

    Created BaseEntities

--- src/com/derekhuang/Game/Game.java
+++ src/com/derekhuang/Game/Game.java
@@ -15,11 +15,14 @@ public class Game extends BasicGame{
 
 	@Override
 	public void render(GameContainer container, Graphics g) throws SlickException {
-		g.setBackground(Color.blue);
-		g.setColor(Color.yellow);
-		g.drawRect(400, 300, 200, 200);
-		g.setColor(Color.red);
-		g.drawString("This is weird", 430, 270);
+		for (int x = 0; x < 540; x++){
+			g.setColor(Color.red);
+			g.drawString("This is weird", 430, x);
+			g.setColor(Color.yellow);
+			g.drawRect(0, 0, 960, 540);
+			g.setColor(Color.blue);
+			g.drawRect(400, x, 200, 200);
+		}
 	}
 
 	@Override
